7f300282fe191fbfe8d6ec8479fe10027f0458c4
VedPanse <panse.ved@yandex.com>
2026-05-13 16:17:48 -0700

    Initial Commit

---
+++ Friday/FridayApp.swift
@@ -0,0 +1,17 @@
+//
+//  FridayApp.swift
+//  Friday
+//
+//  Created by Ved Panse on 5/13/26.
+//
+
+import SwiftUI
+
+@main
+struct FridayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
